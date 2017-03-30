a5fb1b18864ef26f610ebe77fd0e7d4b2b8c8997
cpguan <pguan000@gmail.com>
2017-03-30 18:48:51 -0400

    initial

---
+++ tinder/ProfileViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ProfileViewController.swift
+//  tinder
+//
+//  Created by Pan Guan on 3/29/17.
+//  Copyright © 2017 Pan Guan. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, UIViewControllerTransitioningDelegate {
+    
+    var image: UIImage!
+    var animator = Animator()
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onTapNavBar(_ sender: UITapGestureRecognizer) {
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
